7bbc56be5d99161e944bf964ace845061f424544
vasiaplaton <vasia.platon@gmail.com>
2019-12-21 12:58:23 +0300

    first commit

---
+++ effects.ino
@@ -0,0 +1,189 @@
+
+// ****************************** ОГОНЁК ******************************
+int16_t position;
+boolean direction;
+
+void lighter() {
+  FastLED.clear();
+  if (direction) {
+    position++;
+    if (position > NUM_LEDS - 2) {
+      direction = false;
+    }
+  } else {
+    position--;
+    if (position < 1) {
+      direction = true;
+    }
+  }
+  leds[position] = CRGB::White;
+}
+
+// ****************************** СВЕТЛЯЧКИ ******************************
+#define MAX_SPEED 30
+#define BUGS_AMOUNT 3
+int16_t speed[BUGS_AMOUNT];
+int16_t pos[BUGS_AMOUNT];
+CRGB bugColors[BUGS_AMOUNT];
+
+void lightBugs() {
+  if (loadingFlag) {
+    loadingFlag = false;
+    for (int i = 0; i < BUGS_AMOUNT; i++) {
+      bugColors[i] = CHSV(random(0, 9) * 28, 255, 255);
+      pos[i] = random(0, NUM_LEDS);
+      speed[i] += random(-5, 6);
+    }
+  }
+  FastLED.clear();
+  for (int i = 0; i < BUGS_AMOUNT; i++) {
+    speed[i] += random(-5, 6);
+    if (speed[i] == 0) speed[i] += (-5, 6);
+
+    if (abs(speed[i]) > MAX_SPEED) speed[i] = 0;
+    pos[i] += speed[i] / 10;
+    if (pos[i] < 0) {
+      pos[i] = 0;
+      speed[i] = -speed[i];
+    }
+    if (pos[i] > NUM_LEDS - 1) {
+      pos[i] = NUM_LEDS - 1;
+      speed[i] = -speed[i];
+    }
+    leds[pos[i]] = bugColors[i];
+  }
+}
+
+// ****************************** ЦВЕТА ******************************
+byte hue;
+void colors() {
+  hue += 2;
+  CRGB thisColor = CHSV(hue, 255, 255);
+  fillAll(CHSV(hue, 255, 255));
+}
+
+// ****************************** РАДУГА ******************************
+void rainbow() {
+  hue += 2;
+  for (int i = 0; i < NUM_LEDS; i++)
+    leds[i] = CHSV((byte)(hue + i * float(255 / NUM_LEDS)), 255, 255);
+}
+
+// ****************************** КОНФЕТТИ ******************************
+void sparkles() {
+  byte thisNum = random(0, NUM_LEDS);
+  if (getPixColor(thisNum) == 0)
+    leds[thisNum] = CHSV(random(0, 255), 255, 255);
+  fade();
+}
+
+// ****************************** ОГОНЬ ******************************
+#define COOLING  55
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 120
+
+void fire() {
+  random16_add_entropy( random());
+  Fire2012WithPalette(); // run simulation frame, using palette colors
+}
+
+void Fire2012WithPalette()
+{
+  // Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS1];
+
+  // Step 1.  Cool down every cell a little
+  for ( int i = 0; i < NUM_LEDS1; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for ( int k = NUM_LEDS1 - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
+
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if ( random8() < SPARKING ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160, 255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for ( int j = 0; j < NUM_LEDS1; j++) {
+    // Scale the heat value from 0-255 down to 0-240
+    // for best results with color palettes.
+    byte colorindex = scale8( heat[j], 240);
+    CRGB color = ColorFromPalette( gPal, colorindex);
+    int pixelnumber;
+    if ( gReverseDirection ) {
+    pixelnumber = (NUM_LEDS - 1) - j;
+    } else {
+      pixelnumber = j;
+  }
+    leds[pixelnumber] = color;
+    leds[(NUM_LEDS-1)-pixelnumber] = color;
+  }
+}
+
+// *************** ВИНИГРЕТ ***************
+/*void vinigret() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if ((uint32_t)getPixColor(i) == 0) {
+      
+    }
+  }
+}
+*/
+// ****************** СЛУЖЕБНЫЕ ФУНКЦИИ *******************
+void fade() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if ((uint32_t)getPixColor(i) == 0) continue;
+    leds[i].fadeToBlackBy(TRACK_STEP);
+  }
+}
+void autoplay_off_anim(int delayy){
+            for(int i=0; i<NUM_LEDS; i++){
+            leds[i]=CRGB::Red;
+          }
+          FastLED.show();
+          for(int i=NUM_LEDS-1; i>=0; i--){
+            leds[i]=CRGB::Black;
+           delay(delayy);
+          FastLED.show();
+          }
+}
+void autoplay_on_anim(int delayy){
+            for(int i=0; i<NUM_LEDS; i++){
+            leds[i]=CRGB::Black;
+          }
+          FastLED.show();
+          for(int i=0; i<NUM_LEDS; i++){
+            leds[i]=CRGB::Red;
+            delay(delayy);
+          FastLED.show();
+          }
+}
+void autobright_on_anim(int delayy, int maxx){
+              for(int i=0; i<NUM_LEDS; i++){
+            leds[i]=CRGB::Red;
+          }
+          for(int i=0; i<maxx; i++){
+            FastLED.setBrightness(i);
+            delay(delayy);
+          FastLED.show();
+          }
+          
+}
+void autobright_off_anim(int delayy, int maxx){
+              for(int i=0; i<NUM_LEDS; i++){
+            leds[i]=CRGB::Red;
+          }
+          for(int i=maxx; i>0; i--){
+            FastLED.setBrightness(i);
+            delay(delayy);
+          FastLED.show();
+          }
+          
+}
---
+++ svet_test.ino
@@ -0,0 +1,202 @@
+#include <FastLED.h>
+#include "GyverButton.h"
+#include "GyverTimer.h"
+// settings
+#define TRACK_STEP 1 // less - more steps in animation
+#define FIRE_PALETTE 0  // types of fire
+#define AUTOPLAY_TIME 30 // time of change mode
+#define AUTOBRIGHT_TIME 500
+#define NUM_LEDS 12 // number of leds
+#define NUM_LEDS1 6 // 1/2 number of leds for fire
+#define DATA_PIN 9 // pin of ws2812b
+#define UP_PIN 3 // pin of up button
+#define DOWN_PIN 2   // pin of down button   
+#define BRIGHTNESS 250 // std brightness in start
+#define MIN_BRIGHTNESS 2 // min brightness for hand setting
+#define MODES_AMOUNT 7 // number of modes
+#define STD_SPEED 6 // speed of animation bigger - slowly
+// end
+// leds routins
+CRGB leds[NUM_LEDS];
+CRGBPalette16 gPal;
+// buttons
+GButton UP(UP_PIN, LOW_PULL, NORM_OPEN);
+GButton DOWN(DOWN_PIN, LOW_PULL, NORM_OPEN);
+// timer
+GTimer_ms effectTimer(10); //60
+GTimer_ms autoplayTimer((long)AUTOPLAY_TIME * 1000);
+GTimer_ms autobrightTimer(AUTOBRIGHT_TIME);
+// variables
+boolean loadingFlag = true;
+boolean UP_CL; 
+boolean DOWN_CL;
+bool gReverseDirection = false;
+boolean power=1;
+boolean autoplay=0;
+boolean autobright=0;
+int sped=STD_SPEED;
+int brightness=BRIGHTNESS;
+int mode;
+int a=0;
+int prevmode;
+void setup() { 
+      FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  UP.setTimeout(300);
+  UP.setStepTimeout(10);
+  DOWN.setTimeout(300);
+  DOWN.setStepTimeout(10);
+  
+  randomSeed(analogRead(0));
+  
+  if (FIRE_PALETTE == 0) gPal = HeatColors_p;
+  else if (FIRE_PALETTE == 1) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
+  else if (FIRE_PALETTE == 2) gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White);
+  else if (FIRE_PALETTE == 3) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::White);
+
+}
+
+
+// залить все
+void fillAll(CRGB newcolor) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = newcolor;
+  }
+}
+
+// функция получения цвета пикселя по его номеру
+uint32_t getPixColor(int thisPixel) {
+  return (((uint32_t)leds[thisPixel].r << 16) | ((long)leds[thisPixel].g << 8 ) | (long)leds[thisPixel].b);
+}
+
+void Next_mode(){
+   if(mode<MODES_AMOUNT) mode++;
+   loadingFlag = true;
+   FastLED.clear();
+  /// leds[1] = CRGB::Red;
+}
+void Prev_mode(){
+  if(mode>0) mode--;
+  loadingFlag = true;
+  FastLED.clear();
+ /// leds[1] = CRGB::Blue;
+}
+void ButtonTick(){
+   UP.tick();
+      DOWN.tick();
+      UP_CL=UP.isStep();
+      DOWN_CL=DOWN.isStep();
+      if( UP_CL && DOWN_CL){
+        power=0;
+        //delay(1000);
+      }
+      
+      if(UP.hasClicks() && !DOWN_CL  ){
+        byte clicks = UP.getClicks();
+       switch(clicks){
+          case 1:
+          if(!power) power=1;
+          else Next_mode();
+          break;
+          case 2:
+          if(sped>1) sped--;
+          break;
+          case 3:
+          // autobright on
+          autobright_on_anim(5, 50);
+          autobright=1;
+          break;
+          case 4:
+          autoplay_on_anim(50);
+          autoplay=1;
+                    // autoplay on
+          break;
+        }
+      }
+      if(UP_CL  && !DOWN_CL ){
+        if(brightness+1 < 255) brightness += 1;
+      }
+      if(DOWN.hasClicks() && !UP_CL ){
+        byte clicks = DOWN.getClicks();
+       switch(clicks){
+          case 1:
+          if(!power) power=1;
+          else Prev_mode();
+          break;
+          case 2:
+          if(sped<8) sped++;
+          break;
+          case 3:
+          //autobright off
+          autobright_off_anim(5 ,50);
+          autobright=0;
+          break;
+          case 4:
+          autoplay_off_anim(50);
+          autoplay=0;
+   // autoplay off       
+          break;
+        }
+      }
+      if(DOWN_CL && !UP_CL ){
+        if(brightness-1>MIN_BRIGHTNESS) brightness -= 1;
+      }
+ 
+
+  if(power && !autobright)    FastLED.setBrightness(brightness);
+   if(!power && !autobright) FastLED.setBrightness(0);
+  FastLED.show();
+}
+void ModeTick(){
+  if (effectTimer.isReady() && power) {
+      a++;
+  if(mode<6 && ((a%sped)==0)){
+    switch (mode) {
+      case 0: lighter();
+        break;
+      case 1: lightBugs();
+        break;
+      case 2: colors();
+        break;
+      case 3: rainbow();
+        break;
+      case 4: sparkles();
+        break;
+      case 5: fire();
+        break;
+    }
+ }
+  if( mode >= 6 && (a%sped)==0){
+    switch (mode) {
+      case 6:
+      for(int i=0; i<NUM_LEDS; i++) leds[i]=CRGB::White;
+      break;
+      case 7:
+
+      break;
+    }
+  }
+  FastLED.show();
+  }
+//  FastLED.show();
+}
+void autoPlayTick(){
+    if (autoplayTimer.isReady() && autoplay) {// таймер смены режима
+ mode++;
+ if ( mode > MODES_AMOUNT) mode =0;
+  }
+}
+void autoBrightTick(){
+  if(autobrightTimer.isReady() && autobright){
+    // work with photoresist
+    int val;
+   val=analogRead(A6);
+   val=map(val, 0, 1024, MIN_BRIGHTNESS, 255);
+   FastLED.setBrightness(val);
+  }
+}
+void loop() {
+  ButtonTick();
+  ModeTick(); 
+autoPlayTick();
+
+  }
